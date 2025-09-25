e496487a2dd4400c2f5b799d1f713d7afdacfcda
jdsaifi <jdsaifi729@gmail.com>
2025-09-25 20:12:56 +0530

    added cors

--- src/utils/corsOptions.ts
+++ src/utils/corsOptions.ts
@@ -5,6 +5,7 @@ export const corsOptions = {
         'http://localhost:3044',
         'http://localhost:8080',
         'http://localhost:8081',
+        'https://pepeai.saifisolutions.com',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Specify allowed methods
