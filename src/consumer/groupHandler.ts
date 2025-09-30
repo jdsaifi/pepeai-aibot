59080329c25f4046f667b7e1840a2797b3603756
jdsaifi <jdsaifi729@gmail.com>
2025-09-30 10:00:23 +0530

    fixed my chat member issue

--- src/consumer/groupHandler.ts
+++ src/consumer/groupHandler.ts
@@ -6,9 +6,63 @@ import { ConsumeMessage } from 'amqplib';
 import { consoleLog } from '@/utils/consoleLog';
 import bot from '@/bot';
 
+// const data = {
+//     update_id: 578579984,
+//     my_chat_member: {
+//         chat: {
+//             id: -1002925016269,
+//             title: 'Pepeai agent group 1',
+//             username: 'hfhjvbjkv',
+//             type: 'supergroup',
+//         },
+//         from: {
+//             id: 1365355482,
+//             is_bot: false,
+//             first_name: 'Jd',
+//             last_name: 'Saifi',
+//             username: 'jdsaifi',
+//             language_code: 'en',
+//         },
+//         date: 1759206526,
+//         old_chat_member: {
+//             user: {
+//                 id: 7584561322,
+//                 is_bot: true,
+//                 first_name: 'PepeAI Agent Hub',
+//                 username: 'pepeai_agent_hub_bot',
+//             },
+//             status: 'member',
+//         },
+//         new_chat_member: {
+//             user: {
+//                 id: 7584561322,
+//                 is_bot: true,
+//                 first_name: 'PepeAI Agent Hub',
+//                 username: 'pepeai_agent_hub_bot',
+//             },
+//             status: 'administrator',
+//             can_be_edited: false,
+//             can_manage_chat: true,
+//             can_change_info: true,
+//             can_delete_messages: true,
+//             can_invite_users: true,
+//             can_restrict_members: true,
+//             can_pin_messages: true,
+//             can_manage_topics: false,
+//             can_promote_members: false,
+//             can_manage_video_chats: true,
+//             can_post_stories: true,
+//             can_edit_stories: true,
+//             can_delete_stories: true,
+//             is_anonymous: false,
+//             can_manage_voice_chats: true,
+//         },
+//     },
+// };
+
 const upsertGroup = async (message: ConsumeMessage) => {
     const update = JSON.parse(message.content.toString());
-    if (!update.message || !update.message.chat) {
+    if (!update.message || !update.message.chat || !update.my_chat_member) {
         Logger.error({
             event: 'Invalid message format in consumeGroupHandler',
             content: message.content.toString(),
@@ -61,6 +115,63 @@ const upsertGroup = async (message: ConsumeMessage) => {
         // }
     }
 };
+
+// {
+//   event: 'Telegram Webhook Received',
+//   data: {
+//     update_id: 578579983,
+//     my_chat_member: {
+//       chat: {
+//         id: -1002925016269,
+//         title: 'Pepeai agent group 1',
+//         username: 'hfhjvbjkv',
+//         type: 'supergroup'
+//       },
+//       from: {
+//         id: 1365355482,
+//         is_bot: false,
+//         first_name: 'Jd',
+//         last_name: 'Saifi',
+//         username: 'jdsaifi',
+//         language_code: 'en'
+//       },
+//       date: 1759206478,
+//       old_chat_member: {
+//         user: {
+//           id: 7584561322,
+//           is_bot: true,
+//           first_name: 'PepeAI Agent Hub',
+//           username: 'pepeai_agent_hub_bot'
+//         },
+//         status: 'administrator',
+//         can_be_edited: false,
+//         can_manage_chat: true,
+//         can_change_info: false,
+//         can_delete_messages: true,
+//         can_invite_users: true,
+//         can_restrict_members: true,
+//         can_pin_messages: true,
+//         can_manage_topics: false,
+//         can_promote_members: false,
+//         can_manage_video_chats: false,
+//         can_post_stories: false,
+//         can_edit_stories: false,
+//         can_delete_stories: false,
+//         is_anonymous: false,
+//         can_manage_voice_chats: false
+//       },
+//       new_chat_member: {
+//         user: {
+//           id: 7584561322,
+//           is_bot: true,
+//           first_name: 'PepeAI Agent Hub',
+//           username: 'pepeai_agent_hub_bot'
+//         },
+//         status: 'member'
+//       }
+//     }
+//   }
+// }
 
 async function consumeGroupHandler() {
     const channel = await getRabbitMQChannel();
